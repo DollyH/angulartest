b8d5d71193e70a9227f2fc90462a04d400d7edf0
geetn25 <geetn25@users.noreply.github.com>
2016-06-10 17:21:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,66 @@
 (function() {
-// Add your javascript here
-var app = angular.module('recipeModule', []);
+    // Add your javascript here
+    var app = angular.module('recipeModule', []);
 
-app.factory('itemsFactory', ['$http', function($http){
-  var itemsFactory ={
-    itemDetails: function() {
-      return $http(
-      {
-        url: "recipes.json",
-        method: "GET",
-      })
-      .then(function (response) {
-        return response.data;
-        });
-      }
+    app.factory('itemsFactory', ['$http', function($http){
+      var itemsFactory ={
+        itemDetails: function() {
+          return $http(
+          {
+            url: "recipes.json",
+            method: "GET",
+          })
+          .then(function (response) {
+            return response.data;
+            });
+          }
+        };
+        return itemsFactory;
+      
+    }]);
+
+
+    app.controller('RecipeController', ['$scope', 'itemsFactory', function($scope, itemsFactory){
+
+    this.One=false;
+
+    this.showOne = function (){
+      this.One = true;
     };
-    return itemsFactory;
-  
-}]);
 
-
-app.controller('RecipeController', ['$scope', 'itemsFactory', function($scope, itemsFactory){
-  var promise = itemsFactory.itemDetails();
-  this.recipeReviews = [];
+    var promise = itemsFactory.itemDetails();
+    this.recipeReviews = [];
     promise.then(function (data) {
         $scope.itemDetails = data;
-        console.log(data);
     });
+
     $scope.select = function(item) {
       $scope.selected = item;
     }
+
     $scope.selected = {};
     this.review = {};
 
-      this.addReview = function(product) {
-      this.recipeReviews.push(this.review);
+    this.addReview = function(product) {
+      var i;
+      for(i = 0; i < this.recipeReviews.length; i++) {
+        if(this.recipeReviews[i].name === product.name) {
+          this.recipeReviews[i].reviews.push(this.review);
+          break;
+        }
+      }
+
+      if (i == this.recipeReviews.length) {
+        var tempRecipeReview = {
+          name:product.name, 
+          reviews:[]
+        };
+        tempRecipeReview.reviews.push(this.review);
+        this.recipeReviews.push(tempRecipeReview);
+      } 
+          
       this.review = {};
     };
-}]);
-
-  app.controller('ReviewController', function() {
-    this.review = {};
-
-    this.addReview = function(product) {
-      product.reviews.push(this.review);
-
-    };
-  });
+    }]);
 
 })();
